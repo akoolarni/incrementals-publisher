9f3d1b94d9ffaacb360fe2e068cc569df56f4fff
akoolarni <arinpk210@gmail.com>
2023-11-11 18:57:44 +0530

    Added extract-dependancies.js file

---
+++ extract-dependancies.js
@@ -0,0 +1,20 @@
+const { execSync } = require('child_process');
+
+try {
+  const dependencies = execSync('npm list --depth=0 --json').toString();
+  const parsedDependencies = JSON.parse(dependencies);
+  const libraries = Object.keys(parsedDependencies.dependencies);
+  
+ 
+  libraries.forEach((library) => {
+    const version = parsedDependencies.dependencies[library].version;
+    console.log(`<dependency>
+      <groupId>${library}</groupId>
+      <artifactId>${library}</artifactId>
+      <version>${version}</version>
+    </dependency>`);
+  });
+} catch (error) {
+  console.error('Error extracting dependencies:', error.message);
+  process.exit(1);
+}
